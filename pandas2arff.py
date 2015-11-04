9d9ceba6952ea338c1620136b90740def9918a2c
Saurabh Nagrecha <snagrech@nd.edu>
2015-11-04 13:18:31 -0500

    Update pandas2arff.py

--- pandas2arff.py
+++ pandas2arff.py
@@ -12,21 +12,26 @@ def pandas2arff(df,filename,wekaname = "pandasdata",cleanstringdata=True,cleanna
     import re
     
     def cleanstring(s):
-        return re.sub('[^A-Za-z0-9]+', "_", str(s))
-    
+        if s!="?":
+            return re.sub('[^A-Za-z0-9]+', "_", str(s))
+        else:
+            return "?"
+            
     dfcopy = df #all cleaning operations get done on this copy
-    
+
     
     if cleannan!=False:
         dfcopy = dfcopy.fillna(-999999999) #this is so that we can swap this out for "?"
         #this makes sure that certain numerical columns with missing values don't get stuck with "object" type
-    
+ 
     f = open(filename,"w")
     arffList = []
     arffList.append("@relation " + wekaname + "\n")
     #look at each column's dtype. If it's an "object", make it "nominal" under Weka for now (can be changed in source for dates.. etc)
     for i in range(df.shape[1]):
-        if dfcopy.dtypes[i]=='O' or df.columns[i]=="Class" or dfcopy.columns[i]=="CLASS" or df.columns[i]=="class":
+        if dfcopy.dtypes[i]=='O' or (df.columns[i] in ["Class","CLASS","class"]):
+            if cleannan!=False:
+                dfcopy.iloc[:,i] = dfcopy.iloc[:,i].replace(to_replace=-999999999, value="?")
             if cleanstringdata!=False:
                 dfcopy.iloc[:,i] = dfcopy.iloc[:,i].apply(cleanstring)
             _uniqueNominalVals = [str(_i) for _i in np.unique(dfcopy.iloc[:,i])]
@@ -39,18 +44,19 @@ def pandas2arff(df,filename,wekaname = "pandasdata",cleanstringdata=True,cleanna
             arffList.append("@attribute " + df.columns[i] + " real\n") 
             #even if it is an integer, let's just deal with it as a real number for now
     arffList.append("@data\n")           
-    for i in range(df.shape[0]):#instances
+    for i in range(dfcopy.shape[0]):#instances
         _instanceString = ""
         for j in range(df.shape[1]):#features
-                if df.dtypes[j]=='O':
-                    _instanceString+="\"" + str(df.iloc[i,j]) + "\""
+                if dfcopy.dtypes[j]=='O':
+                    _instanceString+="\"" + str(dfcopy.iloc[i,j]) + "\""
                 else:
-                    _instanceString+=str(df.iloc[i,j])
-                if j!=df.shape[1]-1:#if it's not the last feature, add a comma
+                    _instanceString+=str(dfcopy.iloc[i,j])
+                if j!=dfcopy.shape[1]-1:#if it's not the last feature, add a comma
                     _instanceString+=","
         _instanceString+="\n"
         if cleannan!=False:
-            _instanceString.replace("-999999999","?")
+            _instanceString = _instanceString.replace("-999999999.0","?") #for numeric missing values
+            _instanceString = _instanceString.replace("\"?\"","?") #for categorical missing values
         arffList.append(_instanceString)
     f.writelines(arffList)
     f.close()
