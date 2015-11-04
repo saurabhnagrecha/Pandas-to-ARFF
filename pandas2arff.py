4ce0ef02cf9f905f2c0b82e56413c59eaaf1fdaa
Saurabh Nagrecha <snagrech@nd.edu>
2015-11-04 13:02:47 -0500

    Update pandas2arff.py

--- pandas2arff.py
+++ pandas2arff.py
@@ -23,7 +23,7 @@ def pandas2arff(df,filename,wekaname = "pandasdata",cleanstringdata=True,cleanna
     
     f = open(filename,"w")
     arffList = []
-    arffList.append("@relation " + name + "\n")
+    arffList.append("@relation " + wekaname + "\n")
     #look at each column's dtype. If it's an "object", make it "nominal" under Weka for now (can be changed in source for dates.. etc)
     for i in range(df.shape[1]):
         if dfcopy.dtypes[i]=='O' or df.columns[i]=="Class" or dfcopy.columns[i]=="CLASS" or df.columns[i]=="class":
