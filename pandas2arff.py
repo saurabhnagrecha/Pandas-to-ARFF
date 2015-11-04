5426e1611de1d6d3faffe98728a07866fc20032c
Saurabh Nagrecha <snagrech@nd.edu>
2015-11-04 12:56:50 -0500

    Update pandas2arff.py

--- pandas2arff.py
+++ pandas2arff.py
@@ -1,30 +1,57 @@
-def pandas2arff(df,name = "pandasdata",filename):
+def pandas2arff(df,filename,wekaname = "pandasdata",cleanstringdata=True,cleannan=True):
+    """
+    converts the pandas dataframe to a weka compatible file
+    df: dataframe in pandas format
+    filename: the filename you want the weka compatible file to be in
+    wekaname: the name you want to give to the weka dataset (this will be visible to you when you open it in Weka)
+    cleanstringdata: clean up data which may have spaces and replace with "_", special characters etc which seem to annoy Weka. 
+                     To suppress this, set this to False
+    cleannan: replaces all nan values with "?" which is Weka's standard for missing values. 
+              To suppress this, set this to False
+    """
+    import re
+    
+    def cleanstring(s):
+        return re.sub('[^A-Za-z0-9]+', "_", s)
+    
+    if cleannan!=False:
+        dfcopy = dfcopy.fillna(-999999999) #this is so that we can swap this out for "?"
+        #this makes sure that certain numerical columns with missing values don't get stuck with "object" type
+    
+    dfcopy = df #all cleaning operations get done on this copy
+    
     f = open(filename,"w")
     arffList = []
     arffList.append("@relation " + name + "\n")
     #look at each column's dtype. If it's an "object", make it "nominal" under Weka for now (can be changed in source for dates.. etc)
     for i in range(df.shape[1]):
-        if df.dtypes[i]=='O' or df.columns[i]=="Class" or df.columns[i]=="CLASS" or df.columns[i]=="class":
-            _uniqueNominalVals = [str(_i) for _i in np.unique(df.iloc[:,i])]
+        if dfcopy.dtypes[i]=='O' or df.columns[i]=="Class" or dfcopy.columns[i]=="CLASS" or df.columns[i]=="class":
+            if cleanstringdata!=False:
+                dfcopy.iloc[:,i] = dfcopy.iloc[:,i].apply(cleanstring)
+            _uniqueNominalVals = [str(_i) for _i in np.unique(dfcopy.iloc[:,i])]
             _uniqueNominalVals = ",".join(_uniqueNominalVals)
             _uniqueNominalVals = _uniqueNominalVals.replace("[","")
             _uniqueNominalVals = _uniqueNominalVals.replace("]","")
-            _uniqueValuesString = "{" + _uniqueNominalVals +"}"
+            _uniqueValuesString = "{" + _uniqueNominalVals +"}" 
             arffList.append("@attribute " + df.columns[i] + _uniqueValuesString + "\n")
         else:
-            arffList.append("@attribute " + df.columns[i] + " integer\n")
-    arffList.append("@data\n")
+            arffList.append("@attribute " + df.columns[i] + " real\n") 
+            #even if it is an integer, let's just deal with it as a real number for now
+    arffList.append("@data\n")           
     for i in range(df.shape[0]):#instances
         _instanceString = ""
         for j in range(df.shape[1]):#features
                 if df.dtypes[j]=='O':
-                    _instanceString+="\"" + (df.iloc[i,j]) + "\""
+                    _instanceString+="\"" + str(df.iloc[i,j]) + "\""
                 else:
                     _instanceString+=str(df.iloc[i,j])
                 if j!=df.shape[1]-1:#if it's not the last feature, add a comma
                     _instanceString+=","
         _instanceString+="\n"
+        if cleannan!=False:
+            _instanceString.replace("-999999999","?")
         arffList.append(_instanceString)
     f.writelines(arffList)
     f.close()
+    del dfcopy
     return True
